442f810dfee08210d75f4f5b1963c1aed29bc1c7
alexanderdombroski <dom21013@byui.edu>
2025-04-05 13:10:00 -0600

    Added Starting Styles

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -7,27 +7,32 @@ import { titleCase } from "./string.js";
  * @returns {string} The navigation menu.
  */
 const getNav = (isLoggedIn, isOwner) => {
-    const nav = `
-        <nav>
-            ${isLoggedIn ? '<a href="/account">Account</a>' : ''}
-            ${isLoggedIn ? '<a href="/account/logout">Log Out</a>' : '<a href="/account/login">Log In</a>'}
-            ${isOwner ? '<a href="/vehicle/new">Add Vehicle Listing</a>' : ""}
-            ${isOwner ? '<a href="/vehicle/type">Edit Categories</a>' : ""}    
-            ${isOwner ? '<a href="/account/manage">Manage Users</a>' : ""}
-        </nav>
-    `;
-    
-    return nav;
+	const nav = `
+		<nav>
+			<ul>
+				${isLoggedIn ? '<li><a href="/account">Account</a></li>' : ''}
+				${isLoggedIn ? '<li><a href="/account/logout">Log Out</a>' : '<li><a href="/account/login">Log In</a></li>'}
+				<li><a href="/#">About</a>
+				${isOwner ? '<li><a href="/vehicle/new">Add Vehicle Listing</a></li>' : ""}
+				${isOwner ? '<li><a href="/vehicle/type">Edit Categories</a></li>' : ""}    
+				${isOwner ? '<li><a href="/account/manage">Manage Users</a></li>' : ""}
+			</ul>
+		</nav>
+	`;
+	
+	return nav;
 };
 
 const getVehicleTypeNav = (types) => {
-    return `
-        <nav>
-            <a href="/vehicle">All</a>
-            ${types.map(t => `<a href="/vehicle/type/${t.category_id}">${titleCase(t.name)}</a>`).join('')}
-            <a href="/vehicle/uncategorized">Misc</a>
-        </nav>
-    `;
+	return `
+		<nav>
+			<ul>
+				<li><a href="/vehicle">All</a></li>
+				${types.map(t => `<li><a href="/vehicle/type/${t.category_id}">${titleCase(t.name)}</a></li>`).join('')}
+				<li><a href="/vehicle/uncategorized">Misc</a></li>
+			</ul>
+		</nav>
+	`;
 }
 
 export { getNav, getVehicleTypeNav };
